d9271c24b45588bc29201abaf733f39e132a9dcd
Stanislav Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2023-01-12 00:15:08 +0300

    Fixed not working mapped types

--- src/modules/tech-check-template/dto/update-tech-check-template.dto.ts
+++ src/modules/tech-check-template/dto/update-tech-check-template.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateTechCheckTemplateDto } from './create-tech-check-template.dto';
 
 export class UpdateTechCheckTemplateDto extends PartialType(CreateTechCheckTemplateDto) {}
